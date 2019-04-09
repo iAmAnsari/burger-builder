1d4b8f591d5e2c7894d4479351e2120cfbe5e5b3
Adnan Ansari <36989931+adnan07870@users.noreply.github.com>
2019-04-09 23:39:51 +0530

    test

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+	salad: 5,
+	cheese: 5,
+	meat: 10,
+	sauce: 5
+}	
 
 class BurgerBuilder extends Component {
 
@@ -10,14 +18,53 @@ class BurgerBuilder extends Component {
 			sauce: 0,
 			cheese: 0,
 			meat: 0
-		}
+		},
+		totalPrice: 10
+	}
+    
+	addIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		const updateCount = oldCount + 1;
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+		updatedIngredients[type] = updateCount;
+		const priceAddition = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice + priceAddition;
+		this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+	}
+
+	removeIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		const updateCount = oldCount - 1;
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+		updatedIngredients[type] = updateCount;
+		const priceDeduction = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice - priceDeduction;
+		this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
 	}
 
 	render() {
+
+		const disabledInfo = {
+			...this.state.ingredients
+		};
+
+		for(let key in disabledInfo) {
+			disabledInfo[key] = disabledInfo[key] <= 0;
+		}
+
 		return (
 			<Aux>
 				<Burger ingredients={this.state.ingredients} />
-				<div>Build Controls</div>
+				<BuildControls
+					ingredientAdded={this.addIngredientHandler}
+					ingredientRemoved={this.removeIngredientHandler}
+					disabled={disabledInfo} />
 			</Aux>
 		);
 	}
