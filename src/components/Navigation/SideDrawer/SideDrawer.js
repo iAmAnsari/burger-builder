bf009991151a9ccff11176aff3d650883458f223
Adnan Ansari <36989931+adnan07870@users.noreply.github.com>
2019-04-11 15:28:35 +0530

    test

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import classes from './SideDrawer.css';
+import Aux from '../../../hoc/Auxiliary';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
 const sideDrawer = (props) => {
+
+	let attachClasses = [classes.SideDrawer, classes.Close];
+
+	if(props.open) {
+		attachClasses = [classes.SideDrawer, classes.Open];
+	}
+
 	return (
-		<div className={classes.SideDrawer} >
-			<Logo height="11%" marginBottom="32px" />
-			<nav>
-				<NavigationItems />
-			</nav>
-		</div>
+		<Aux>
+			<Backdrop show={props.open} clicked={props.closed} />
+			<div className={attachClasses.join(' ')} >
+				<Logo height="11%" marginBottom="32px" />
+				<nav>
+					<NavigationItems />
+				</nav>
+			</div>
+		</Aux>
 	);
 }
 
